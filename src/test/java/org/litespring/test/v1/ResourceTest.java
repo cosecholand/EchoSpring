42402cdd3a180c0fd07a87e24c9f11c473fcfeb1
cosecholand <w3cjava@gmail.com>
2019-10-17 18:53:10 +0800

    Resource测试用例

---
+++ src/test/java/org/litespring/test/v1/ResourceTest.java
@@ -0,0 +1,52 @@
+package org.litespring.test.v1;
+
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.litespring.core.io.ClassPathResource;
+import org.litespring.core.io.FileSystemResource;
+import org.litespring.core.io.Resource;
+
+public class ResourceTest {
+
+	@Test
+	public void testClassPathResource() throws Exception {
+
+		Resource r = new ClassPathResource("petstore-v1.xml");
+
+		InputStream is = null;
+
+		try {
+			is = r.getInputStream();
+			// 注意：这个测试其实并不充分！！
+			Assert.assertNotNull(is);
+		} finally {
+			if (is != null) {
+				is.close();
+			}
+		}
+
+	}
+
+	@Test
+	public void testFileSystemResource() throws Exception {
+
+		Resource r = new FileSystemResource(
+				"D:\\Workspaces\\MyEclipse Professional 2018\\EchoSpring\\src\\test\\resources\\petstore-v1.xml");
+
+		InputStream is = null;
+
+		try {
+			is = r.getInputStream();
+			// 注意：这个测试其实并不充分！！
+			Assert.assertNotNull(is);
+		} finally {
+			if (is != null) {
+				is.close();
+			}
+		}
+
+	}
+
+}
