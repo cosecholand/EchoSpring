a7725f19c420da23535af8256928f0553d523fd6
cosecholand <w3cjava@gmail.com>
2019-10-17 15:57:30 +0800

    测试驱动获取Bean

---
+++ src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -0,0 +1,19 @@
+package org.litespring.beans.factory.support;
+
+import org.litespring.beans.BeanDefinition;
+import org.litespring.beans.factory.BeanFactory;
+
+public class DefaultBeanFactory implements BeanFactory {
+
+	public DefaultBeanFactory(String configFile) {
+	}
+
+	public BeanDefinition getBeanDefinition(String beanId) {
+		return null;
+	}
+
+	public Object getBean(String beanId) {
+		return null;
+	}
+
+}
