523e78eb0f00e07893307b002b2779ee98cbdf28
cosecholand <w3cjava@gmail.com>
2019-10-17 19:08:52 +0800

    增加FileSystemXmlApplicationContext测试用例

--- src/test/java/org/litespring/test/v1/ApplicationContextTest.java
+++ src/test/java/org/litespring/test/v1/ApplicationContextTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.litespring.context.ApplicationContext;
 import org.litespring.context.support.ClassPathXmlApplicationContext;
+import org.litespring.context.support.FileSystemXmlApplicationContext;
 import org.litespring.service.v1.PetStoreService;
 
 public class ApplicationContextTest {
@@ -17,9 +18,9 @@ public class ApplicationContextTest {
     @Test 
 	public void testGetBeanFromFileSystemContext(){
 	    //注意啊，这里仍然是hardcode了一个本地路径，这是不好的实践!! 如何处理，留作作业
-		/*ApplicationContext ctx = new FileSystemXmlApplicationContext("C:\\Users\\liuxin\\git-litespring\\src\\test\\resources\\petstore-v1.xml");
+		ApplicationContext ctx = new FileSystemXmlApplicationContext("D:\\\\Workspaces\\\\MyEclipse Professional 2018\\\\EchoSpring\\\\src\\\\test\\\\resources\\\\petstore-v1.xml");
 		PetStoreService petStore = (PetStoreService)ctx.getBean("petStore");
-		Assert.assertNotNull(petStore);*/
+		Assert.assertNotNull(petStore);
 		
 	}
 
